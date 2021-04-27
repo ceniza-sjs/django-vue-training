b1e601707148ce6339a0386fd666899349b9e27a
ceniza-sjs <stevjocen@outlook.ph>
2021-04-27 16:47:48 +0800

    Add user and track relationship.

--- app/tracks/schema.py
+++ app/tracks/schema.py
@@ -3,28 +3,38 @@ import graphene
 from graphene_django import DjangoObjectType
 from .models import Track
 
+
 class TrackType(DjangoObjectType):
     class Meta:
         model = Track
-    
+
+
 class Query(graphene.ObjectType):
     tracks = graphene.List(TrackType)
 
     def resolve_tracks(self, info):
         return Track.objects.all()
 
+
 class CreateTrack(graphene.Mutation):
     track = graphene.Field(TrackType)
 
-    class Arguments: 
+    class Arguments:
         title = graphene.String()
         description = graphene.String()
         url = graphene.String()
 
     def mutate(self, info, title, description, url):
-        track = Track(title=title, description=description, url=url)
+        user = info.context.user
+
+        if user.is_anonymous:
+            raise Exception('Login to add a track!')
+
+        track = Track(title=title, description=description,
+                      url=url, posted_by=user)
         track.save()
         return CreateTrack(track=track)
-        
+
+
 class Mutation(graphene.ObjectType):
     create_track = CreateTrack.Field()
