01a70441a45d88e35c13a07d38e992f40ba4fd08
ceniza-sjs <stevjocen@outlook.ph>
2021-04-29 12:57:47 +0800

    temp.

---
+++ client/pages/register.vue
@@ -0,0 +1,13 @@
+<template>
+  <form>
+    <input type="email" name="email" id="email" />
+    <input type="text" name="username" id="username" />
+    <input type="password" name="password" id="password" />
+  </form>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
